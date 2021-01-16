d181f6cbff6ccb809876beb2928572653273ee33
Harsh Mistry <hhrshmistry@gmail.com>
2021-01-17 03:01:14 +0530

    call to minions API successfully

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(text) {
-    return serverURL + "?" + text;
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
 }
- 
+
 function errorHandler() {
     console.log("error occured!", error);
-    alert("something wrong with server: try again after some time");
+    alert("Something wrong with server: try again after some time");
 }
 
 function clickHandler() {
-    // outputDiv.innerText = "Banana Speak " + txtInput.value;
     var inputText = txtInput.value; // taking input
 
-    // calling server for processing
+    // calling server for processing, for details go to session 4 javascript in browser -> time 2:34:00
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerHTML = translatedText; // output
+    })
     .catch(errorHandler)
 
 }
