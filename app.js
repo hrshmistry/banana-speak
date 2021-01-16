3b6608784083406700db14be4ef97a8f58397925
Harsh Mistry <hhrshmistry@gmail.com>
2021-01-17 02:43:59 +0530

    call to mock server from js and error handling

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
+function getTranslationURL(text) {
+    return serverURL + "?" + text;
+}
+ 
+function errorHandler() {
+    console.log("error occured!", error);
+    alert("something wrong with server: try again after some time");
+}
 
 function clickHandler() {
-    outputDiv.innerText = "ajcnacancalcna " + txtInput.Value;
+    // outputDiv.innerText = "Banana Speak " + txtInput.value;
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
+
 }
 
 btnTranslate.addEventListener("click", clickHandler);
